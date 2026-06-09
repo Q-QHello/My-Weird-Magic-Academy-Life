514f9ebd5bee7a65a64d7a79fa61725ce7add22e
Q-QHello <167091855+Q-QHello@users.noreply.github.com>
2026-06-09 17:19:52 +0800

    Create 5 scenes
    Create 4 pixel art classroom scenes and 1 dormitory scene.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -9,7 +9,11 @@ public class CameraFollow : MonoBehaviour
     void LateUpdate()
     {
         Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        Vector3 smoothedPosition = Vector3.Lerp(
+            transform.position,
+            desiredPosition,
+            smoothSpeed * Time.deltaTime);
+
         transform.position = smoothedPosition;
     }
 }
