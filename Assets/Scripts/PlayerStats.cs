9713dc3d5cdc66c2664778ee8f72ad695630d60a
Q-QHello <167091855+Q-QHello@users.noreply.github.com>
2026-06-15 12:56:43 +0800

    Implemented basic player statistics system and stats UI panel
    Added Arcane, Potion, and Rune attributes along with a statistics UI panel. Players can view their current attributes through the Stats menu. This system will be used by future gameplay features such as studying, potion brewing, rune carving, and semester progression.

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+// Stores player progression statistics
+public class PlayerStats : MonoBehaviour
+{
+// Magic proficiency
+public static int arcane = 0;
+
+
+// Potion brewing proficiency
+public static int potion = 0;
+
+// Rune carving proficiency
+public static int rune = 0;
+
+
+}
